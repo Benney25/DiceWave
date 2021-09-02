708ca729c8ff407093ae25eec0ea9b439beb72ec
Benney25 <glickbbm@hotmail.com>
2021-09-02 00:55:25 -0700

    cleaned comments, reorganized and pruned code, fixed single word call error. Now calls 6 individual words and prints the number of times the program / function has ran

--- Logic.py
+++ Logic.py
@@ -1,32 +1,33 @@
 import random
 import math
 
-# first step is to get the number of lines in the file and store as a var
-file = open("corncob_caps.txt", "r")
-z = 0
-for line in file:
-    if line != "\n":
-        z += 1
-file.close()
-
-# second step is to return a random integer between 1 and variable, and store the results as a separate var
-y = random.uniform(1, z)
-x = math.floor(y)
-
-# Third step is to use the new int to get a random word from the file.
-# https://www.kite.com/python/answers/how-to-read-specific-lines-of-a-text-file-in-python
 def getword():
-    a_file = open("corncob_caps.txt")
+
+# get the number of newlines in the text file
+    file = open("corncob_caps.txt", "r")
+    z = 0
+    for line in file:
+        if line != "\n":
+            z += 1
+    file.close()
+
+# pick a line from the file, store that line in a variable
+    y = random.uniform(1, z)
+    x = math.floor(y)
+
+# use stored line to get the word on that line
+    file = open("corncob_caps.txt")
     lines_to_read = [x]
-    for position, line in enumerate(a_file):
+    for position, line in enumerate(file):
         if position in lines_to_read:
             print(line)
-    # file = open('corncob_caps.txt')
-    # for position, x in enumerate(file):
-    #     if 1 in x:
-    #         print(x)
+    file.close()
 
-getword()
-
-
-# the fourth step will be to eventually combine everything into a function that will be called (var) times
+# call the word (input-var), or (static-6) times.
+def getpass():
+    g = 0
+    for i in range(6):
+        getword()
+        g += 1
+    print(g)
+getpass()
--- test.py
+++ test.py
@@ -1,9 +1,39 @@
-a_file = open("test_file.txt")
+import random
+import math
 
-lines_to_read = [0, 2]
 
-for position, line in enumerate(a_file):
 
-    if position in lines_to_read:
 
-        print(line)
+def getword():
+
+# get the number of newlines in the text file
+    file = open("corncob_caps.txt", "r")
+    z = 0
+    for line in file:
+        if line != "\n":
+            z += 1
+    file.close()
+
+# pick a line from the file, store that line in a variable
+    y = random.uniform(1, z)
+    x = math.floor(y)
+
+# use stored line to get the word on that line
+    file = open("corncob_caps.txt")
+    lines_to_read = [x]
+    for position, line in enumerate(file):
+        if position in lines_to_read:
+            print(line)
+    file.close()
+
+# getword()
+
+def getpass():
+    # getword()
+    g = 0
+    # getword()
+    for i in range(6):
+        getword()
+        g += 1
+    print(g)
+getpass()
