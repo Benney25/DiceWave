7f7040bd99a5868e7c511f609d52bc5ea3e351b2
Benney25 <glickbbm@hotmail.com>
2021-09-02 05:56:30 -0700

    can now strip newline from data while retrieving it from the txt file

--- test.py
+++ test.py
@@ -15,19 +15,29 @@ def getword():
     y = random.uniform(1, z)
     x = math.floor(y)
 
-# use stored line to get the word on that line
+# use stored line to return the word on that line
     file = open("corncob_caps.txt")
     lines_to_read = [x]
     for position, line in enumerate(file):
         if position in lines_to_read:
-            print(line)
+            word = line.rstrip('\r\n')
+            return(word)
     file.close()
 
 # call the word (input-var), or (static-6) times.
 def getpass():
-    g = 0
+    g = getword()
+    g = str()
     for i in range(6):
-        getword()
-        g += 1
-    print(g)
-getpass()
+        g = g + getword()
+    return{g}
+
+# print("".join(getpass()))
+print(getpass())
+
+# def concat():
+#     passwords = getpass()
+#     passwords.strip()
+#     print(passwords)
+
+# concat()
